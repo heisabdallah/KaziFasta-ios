55f05c74e48ed1e91b10c414c46f9560f47dbae0
Abdallah Khalfan <heisabdallah@gmail.com>
2023-10-05 21:04:53 +0300

    Initial Commit

---
+++ kazifasta/kazifastaApp.swift
@@ -0,0 +1,17 @@
+//
+//  kazifastaApp.swift
+//  kazifasta
+//
+//  Created by Abdallah Khalfan on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct kazifastaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
